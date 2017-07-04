5454ca57f53327b394a8273f9cc01581be747d08
Aytekin Yaliz <aytekinyaliz@gmail.com>
2017-07-04 19:10:36 +0100

    Update cityReducer.ts

--- src/reducers/cityReducer.ts
+++ src/reducers/cityReducer.ts
@@ -1,3 +1,40 @@
+import { CityActionType } from '../actions/cityActions';
+import initialStates from './initialStates';
+import * as types from '../actions/actionTypes';
+import City from '../models/City';
+
+export default function cityReducer (
+    state: City[] = initialStates.cities, 
+    action: CityActionType
+): City[] {
+    switch (action.type) {
+        case types.GET_CITIES: {
+            return [
+                new City({id: 6, text: 'Ankara'}),
+                new City({id: 7, text: 'Antalya'})
+            ];
+        }
+        case types.LOG_CITYNAME: {
+            console.log(`City count: ${state.length}`);
+            
+            return state;
+        }
+        // case types.GET_CITIES_SUCCESS: {
+        //     let cities = action.data.map(city => {
+        //         return new City(city);
+        //     });
+
+        //     return {
+        //         ...state,
+        //         cities
+        //     };
+        // }
+        default:
+            return state;
+    }
+}
+
+
 // import initialStates from './initialStates';
 // import * as types from '../actions/actionTypes';
 // import City from '../models/City';
@@ -18,38 +55,3 @@
 //             return state;
 //     }
 // }
-
-
-
-
-
-import { CityActionType } from '../actions/cityActions';
-import initialStates from './initialStates';
-import * as types from '../actions/actionTypes';
-import City from '../models/City';
-
-export default function cityReducer (
-    state: City[] = initialStates.cities, 
-    action: CityActionType
-): City[] {
-    switch (action.type) {
-        case types.GET_CITIES: {
-            return [
-                new City({id: 6, text: 'Ankara'}),
-                new City({id: 7, text: 'Antalya'})
-            ];
-        }
-        // case types.GET_CITIES_SUCCESS: {
-        //     let cities = action.data.map(city => {
-        //         return new City(city);
-        //     });
-
-        //     return {
-        //         ...state,
-        //         cities
-        //     };
-        // }
-        default:
-            return state;
-    }
-}
