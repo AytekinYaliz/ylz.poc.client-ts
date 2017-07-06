4680e72016fc304644c8ecbf5c10bc2a9766dd48
Aytekin Yaliz <aytekinyaliz@gmail.com>
2017-07-06 16:23:28 +0100

    less loader

--- src/components/footer/FooterComponent.tsx
+++ src/components/footer/FooterComponent.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
 
-const FooterComponent = (props: {}) => {
+// const FooterComponent = (props: {}) => {
+//     return (
+//         <footer className="footer">
+//             <div className="container">
+//                 <p className="text-muted">@2017</p>
+//             </div>
+//         </footer>
+//     );
+// };
+
+const FooterComponent: React.StatelessComponent<{}> = () => { 
+    const year = new Date().getFullYear();
+
     return (
         <footer className="footer">
             <div className="container">
-                <p className="text-muted">@2017</p>
+                <p className="text-muted">@{year}</p>
             </div>
         </footer>
     );
