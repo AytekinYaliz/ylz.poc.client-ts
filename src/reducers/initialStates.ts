c0c567312840ea8479991aed221a21a254eb9190
Aytekin Yaliz <aytekinyaliz@gmail.com>
2017-07-07 17:03:32 +0100

    Update initialStates.ts

--- src/reducers/initialStates.ts
+++ src/reducers/initialStates.ts
@@ -1,7 +1,7 @@
 import City from '../models/City';
 
 export default {
-    //currentUser: null,
+    currentUser: { name: 'aytekin' },    // null,
     enthusiasm: {
         languageName: 'TS', 
         enthusiasmLevel: 4
