6fe7a377f9de08b9174526bad27f1c19727f1568
Aytekin Yaliz <aytekinyaliz@gmail.com>
2017-07-07 11:45:56 +0100

    restore index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,23 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
+import './index.less';
+//import '../node_modules/toastr/build/toastr.min.css';
+
+import Routes from './Routes';
+import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/configureStore.dev';
+import {getCities} from './actions/cityActions';
+
+const store = configureStore();
+store.dispatch(getCities());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Routes />
+    </Provider>,
+    document.getElementById('app') as HTMLElement
+);
+
+registerServiceWorker();
