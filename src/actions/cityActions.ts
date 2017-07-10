bd98cc74037acd26b0d12d0637fe5ec32448a492
Aytekin Yaliz <aytekinyaliz@gmail.com>
2017-07-10 15:29:55 +0100

    Update cityActions.ts

--- src/actions/cityActions.ts
+++ src/actions/cityActions.ts
@@ -1,31 +1,5 @@
-// import * as types from './actionTypes';
-
-
-// export interface IncrementEnthusiasm {
-//     type: types.INCREMENT_ENTHUSIASM;
-// }
-// export interface DecrementEnthusiasm {
-//     type: types.DECREMENT_ENTHUSIASM;
-// }
-
-// export type EnthusiasmAction = IncrementEnthusiasm | DecrementEnthusiasm;
-
-// export function incrementEnthusiasm(): IncrementEnthusiasm {
-//     return {
-//         type: types.INCREMENT_ENTHUSIASM
-//     };
-// }
-// export function decrementEnthusiasm(): DecrementEnthusiasm {
-//     return {
-//         type: types.DECREMENT_ENTHUSIASM
-//     };
-// }
-
-
-
-
 import * as types from './actionTypes';
-import IActionType from './IActionType';
+import {IActionType} from './IActionType';
 
 export interface IGetCities extends IActionType {}
 export interface ILogCityName extends IActionType {}
@@ -42,26 +16,3 @@ export function logCityName(): ILogCityName {
         type: types.LOG_CITYNAME
     };
 }
-
-
-// export const getCitiesBegin = () => {
-//     return {type: types.GET_CITIES_BEGIN};
-// };
-// export const getCitiesSuccess = (data) => {
-//     return {type: types.GET_CITIES_SUCCESS, data};
-// };
-// export const getCitiesError = (error) => {
-//     return {type: types.GET_CITIES_ERROR, error};
-// };
-// export function getCities() {
-//     return function(dispatch) {
-//         dispatch(getCitiesBegin());
-
-//         api.getApi(
-//             dispatch,
-//             getConfig(configTypes.apiBaseUrl) + getConfig(configTypes.citiesEndpoint)
-//         )
-//             .then(data => dispatch(getCitiesSuccess(data)))
-//             .catch(error => dispatch(getCitiesError(error)));
-//     };
-// }
