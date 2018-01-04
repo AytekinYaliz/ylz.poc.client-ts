cdd0c5c40d2c4b8bfd3d0bea04891b85801b5ee0
Aytekin Yaliz <aytekinyaliz@gmail.com>
2018-01-04 18:07:20 +0000

    Update js.js

--- examples/js.js
+++ examples/js.js
@@ -1,3 +1,38 @@
+### SCOPE: ###
+The scope of a variable: The scope of a variable are the locations where it is accessible.
+Variables in JavaScript are lexically scoped, so the static structure of a program determines the scope of a variable (it is not influenced by, say, where a function is called from).
+Variables are function-scoped: only functions introduce new scopes; blocks are ignored when it comes to scoping.
+
+IIFE:
+An IIFE enables you to attach private data to a function
+function f() {
+    if (condition) {
+        var tmp = "...";
+    }
+    // tmp still exists here
+}
+
+
+var result = [];
+for (var i=0; i < 5; i++) {
+    result.push(function () { return i });  // (1)
+}
+console.log(result[1]()); // 5 (not 1)
+console.log(result[3]()); // 5 (not 3)
+
+for (var i=0; i < 5; i++) {
+    (function () {
+        var i2 = i; // copy current i
+        result.push(function () { return i2 });
+    }());
+}
+
+### HOISTING: ###
+JavaScript hoists all variable declarations, it moves them to the beginning of their direct scopes.
+The complete function is hoisted, not just the creation of the variable in which it is stored.
+
+- - -
+
 // CommonJS -------------------------------------------------------------------
 // testA.js                         |   // testB.js
 exports.funcA = () => {             |   module.exports = () => {
